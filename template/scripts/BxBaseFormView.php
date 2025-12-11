4246096840b39f4051d8bcb270f48bf33f28ea37
AntonLV <vlnotna@gmail.com>
2025-12-11 07:30:03 +0300

    Ticket #5291 - Improve uploader and storage collaboration. P3.

--- template/scripts/BxBaseFormView.php
+++ template/scripts/BxBaseFormView.php
@@ -418,7 +418,9 @@ class BxBaseFormView extends BxDolForm
                 $aInput['value'] = '';
                 $aInput['values'] = '';
                 $aInput['values_src'] = '';
-                if(!empty($aInput['content_id']) && ($sUpr = reset($aInput['uploaders'])) && isset($aInput['storage_object']) && ($sStg = $aInput['storage_object']) && ($oUploader = BxDolUploader::getObjectInstance($sUpr, $sStg, genRndPwd(8))) !== false){
+                if(!empty($aInput['content_id']) && ($sUpr = reset($aInput['uploaders'])) && ($sStg = $aInput['storage_object'] ?? '') && ($oUploader = BxDolUploader::getObjectInstance($sUpr, $sStg, genRndPwd(8))) !== false) {
+                    $oUploader->setMultiple($aInput['multiple'] ?? true);
+
                     $sData = $oUploader->getGhostsWithOrder((int)bx_get_logged_profile_id(), 'json', $aInput['images_transcoder'], $aInput['content_id']);
                     if($sData && ($aData = json_decode($sData, true)) && !empty($aData['g']) && is_array($aData['g'])) {
                         $aInput['values_src'] = array_values($aData['g']);
