a338d27b0647e39539ac4f8e9ba62806c21467ac
Aleksandr T <at@unacms.com>
2026-01-27 14:23:56 +0300

    Upgrade script from 15.0.0-A2 to 15.0.0-B1

--- upgrade/files/15.0.0.A2-15.0.0.B1/files_delete.php
+++ upgrade/files/15.0.0.A2-15.0.0.B1/files_delete.php
@@ -1,6 +1,9 @@
 <?php
 
-$aFilesDelete = array(
-
-);
+$aFilesDelete = [
+    'plugins/paragonie/sodium_compat/lib/namespaced.php',
+    'plugins/stripe/stripe-php/API_VERSION',
+    'plugins_public/filepond/locale/sr-rs',
+    'template/privacy_icon.html',
+];
 
