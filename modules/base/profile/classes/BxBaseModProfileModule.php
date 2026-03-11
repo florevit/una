c6e25c7d60932d0a6f7e59dd85e44ff59ac9e050
AntonLV <vlnotna@gmail.com>
2026-03-11 15:56:44 +0300

    Ticket #5419 - Timeline: Add "Profile Created" event.

--- modules/base/profile/classes/BxBaseModProfileModule.php
+++ modules/base/profile/classes/BxBaseModProfileModule.php
@@ -2527,6 +2527,8 @@ class BxBaseModProfileModule extends BxBaseModGeneralModule implements iBxDolCon
             $aResult['meta'] = $oMetaMenu->getCodeAPI();
         }
 
+        $this->decodeDataAPICommonFields($aResult, $aData, $aParams);
+
         return $aResult;
     }
 
