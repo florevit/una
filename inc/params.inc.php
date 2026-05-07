f9a87bc6e8b00ff16b304b1f65e5e42336a6ce44
Aleksandr Trofimov <at@unacms.com>
2026-05-07 18:39:42 +0300

    #5479 Environment variables as config

--- inc/params.inc.php
+++ inc/params.inc.php
@@ -71,7 +71,7 @@ define('BX_PAYMENT_EMPTY_ID', 0);
 //--- Studio settings ---//
 if (!defined('BX_DOL_STUDIO_FOLDER')) {
     define('BX_DOL_STUDIO_FOLDER', 'studio');
-
+}
 define('BX_DOL_URL_STUDIO', BX_DOL_URL_ROOT . BX_DOL_STUDIO_FOLDER . '/');
 define('BX_DOL_URL_STUDIO_BASE', BX_DOL_URL_STUDIO . 'template/');
 
