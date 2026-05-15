ffdef63d368f0f26e29c1b1758e1e563ac939314
AntonLV <vlnotna@gmail.com>
2026-05-15 14:43:42 +0300

    Ticket #4577 - Change vendor for all modules.

--- modules/boonex/intercom/install/config.php
+++ modules/boonex/intercom/install/config.php
@@ -17,12 +17,12 @@ $aConfig = array(
     'name' => 'bx_intercom',
     'title' => 'Intercom',
     'note' => 'Intercom integration.',
-    'version' => '13.0.1.DEV',
-    'vendor' => 'BoonEx',
+    'version' => '15.0.0.DEV',
+    'vendor' => 'UNA INC',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
-        '13.0.x'
+        '15.0.x'
     ),
 
     /**
--- modules/boonex/mailchimp/install/config.php
+++ modules/boonex/mailchimp/install/config.php
@@ -17,12 +17,12 @@ $aConfig = array(
     'name' => 'bx_mailchimp',
     'title' => 'Mailchimp',
     'note' => 'Mailchimp integration.',
-    'version' => '13.0.2.DEV',
-    'vendor' => 'BoonEx',
+    'version' => '15.0.0.DEV',
+    'vendor' => 'UNA INC',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
-        '13.0.x'
+        '15.0.x'
     ),
 
     /**
