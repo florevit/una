fbc12a4bcd04ea11db1b69898adcd73b49219d51
AntonLV <vlnotna@gmail.com>
2026-06-01 15:55:16 +0300

    Ticket #4577 - Change vendor for all modules.

--- modules/boonex/wiki/install/config.php
+++ modules/boonex/wiki/install/config.php
@@ -17,12 +17,12 @@ $aConfig = array(
     'name' => 'bx_wiki',
     'title' => 'Wiki',
     'note' => 'Wiki.',
-    'version' => '13.0.2.DEV',
-    'vendor' => 'BoonEx',
+    'version' => '15.0.0.DEV',
+    'vendor' => 'UNA INC',
     'help_url' => 'http://feed.una.io/?section={module_name}',
 
     'compatible_with' => array(
-        '13.0.x'
+        '15.0.x'
     ),
 
     /**
