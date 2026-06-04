486da558430e088dc778687b10b4737a5ed1994b
AntonLV <vlnotna@gmail.com>
2026-06-04 08:01:52 +0300

    Modules updates.

--- modules/boonex/tasks/updates/15.0.2_15.0.3/install/sql/install.sql
+++ modules/boonex/tasks/updates/15.0.2_15.0.3/install/sql/install.sql
@@ -31,9 +31,9 @@ CREATE TABLE IF NOT EXISTS `bx_tasks_pre_values` (
   `value` varchar(255) NOT NULL DEFAULT '',
   `order` int(11) NOT NULL DEFAULT '0',
   `title` varchar(255) NOT NULL DEFAULT '',
-  `color` text NOT NULL DEFAULT '',
+  `color` varchar(128) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `list_value` (`list`, `value`)
+  UNIQUE KEY `list_value` (`list`, `value`(191))
 );
 
 CREATE TABLE IF NOT EXISTS `bx_tasks_filters` (
